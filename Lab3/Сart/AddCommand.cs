5ec3fd078ecd41b1d8c11daddde2d5d2fb2d50e8
lexqzmel <160411072+lexqzmel@users.noreply.github.com>
2024-03-25 14:21:56 +0200

    Add files via upload

---
+++ Lab3/Сart/AddCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сart
+{
+    public class AddCommand : ICommand
+    {
+        private CartProduct product;
+        public AddCommand(CartProduct prod)
+        {
+            product = prod;
+        }
+        public void Execute()
+        {
+            product.Add();
+        }
+    }
+}
